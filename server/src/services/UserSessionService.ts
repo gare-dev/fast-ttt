2790df621d0725f0367bb18015ede2a82409b22f
Guilherme Gare <ggare.contato@gmail.com>
2025-07-23 08:44:00 -0300

    fix: fixed ci.yml for installing all dependencies

--- server/src/services/UserSessionService.ts
+++ server/src/services/UserSessionService.ts
@@ -1,4 +1,4 @@
-import validateNickname from "../../utils/validateNickname";
+import validateNickname from "../../utils/validateNickname.js";
 import { BadRequestError } from "../errors/BadRequest.js";
 import { ConflictError } from "../errors/ConflictError.js";
 import { UserSessionRepository } from "../repositories/UserSessionRepository.js";
